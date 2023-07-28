7f45c4df881c48ccf01f8310222b0cc32c801476
Randy Awuri <awuri.randy@yahoo.com>
2023-07-28 02:22:48 +0100

    a simple function

--- functions_and_headers/sum_of_numbers.c
+++ functions_and_headers/sum_of_numbers.c
@@ -7,8 +7,10 @@ int numbers_sum(int a, int b);
  */
 int main(void)
 {
+	/* function call */
 	int result = numbers_sum(2, 3);
-
+	
+	/* print result */
 	printf("The result is %d", result);
 
 	return (0);
