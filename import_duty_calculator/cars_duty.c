c33d2c4a48363c95fddc90573260794c2329982a
randyawuri <awuri.randy@yahoo.com>
2023-09-21 14:41:57 +0100

    cars duty update

--- import_duty_calculator/cars_duty.c
+++ import_duty_calculator/cars_duty.c
@@ -5,13 +5,21 @@
 void duty_calc(float usd_val, float ex_rate, float CIF, float duty, float levy, float cis, float sur, float etl, float vat, float total)
 {
     printf("Exchange rate: %.3f\n", ex_rate);
+    printf("____________________________________\n");
     printf("CIF in Naira: %.2f\n", CIF);
+    printf("....................................\n");
     printf("Duty: %.2f\n", duty);
+    printf("....................................\n");
     printf("CIS: %.2f\n", cis);
+    printf("....................................\n");
     printf("Surcharge: %.2f\n", sur);
+    printf("....................................\n");
     printf("ETLS: %.2f\n", etl);
+    printf("....................................\n");
     printf("Levy: %.2f\n", levy);
+    printf("....................................\n");
     printf("VAT: %.2f\n", vat);
+    printf("____________________________________\n");
 
     printf("TOTAL: %.2f\n", total);
 }
